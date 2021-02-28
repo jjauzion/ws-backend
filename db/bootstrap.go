a3b9aa5f9bf85aa721756ad2ae885af80afce11e
jjauzion <jeremy.jauzion@gmail.com>
2021-02-28 17:15:36 +0100

    error log out of bootstrap

--- db/bootstrap.go
+++ db/bootstrap.go
@@ -3,15 +3,12 @@ package db
 import (
 	"github.com/google/uuid"
 	"github.com/jjauzion/ws-backend/graph/model"
-	"github.com/jjauzion/ws-backend/internal/logger"
-	"go.uber.org/zap"
 	"time"
 )
 
-func Bootstrap(dbh DatabaseHandler, log *logger.Logger) error {
+func Bootstrap(dbh DatabaseHandler) error {
 	err := dbh.Bootstrap()
 	if err != nil {
-		log.Error("bootstrap failed", zap.Error(err))
 		return err
 	}
 	userSimple := model.User{
@@ -22,7 +19,6 @@ func Bootstrap(dbh DatabaseHandler, log *logger.Logger) error {
 	}
 	err = dbh.CreateUser(userSimple)
 	if err != nil {
-		log.Error("bootstrap failed", zap.Error(err))
 		return err
 	}
 	userAdmin := model.User{
@@ -33,7 +29,6 @@ func Bootstrap(dbh DatabaseHandler, log *logger.Logger) error {
 	}
 	err = dbh.CreateUser(userAdmin)
 	if err != nil {
-		log.Error("bootstrap failed", zap.Error(err))
 		return err
 	}
 	dataset := "s3://task1"
@@ -48,7 +43,6 @@ func Bootstrap(dbh DatabaseHandler, log *logger.Logger) error {
 	}
 	err = dbh.CreateTask(task1)
 	if err != nil {
-		log.Error("bootstrap failed", zap.Error(err))
 		return err
 	}
 	return err
