b390b231506d640f54390ed932366d0b6f4a5d70
Karim BENHAMMOU <karim@benhammou.me>
2021-03-07 20:50:43 +0100

    remove old client

--- db/db_test.go
+++ db/db_test.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	elasticsearch7 "github.com/elastic/go-elasticsearch/v7"
 	"github.com/jjauzion/ws-backend/conf"
 	"github.com/jjauzion/ws-backend/internal/logger"
 	"github.com/olivere/elastic/v7"
@@ -18,21 +17,16 @@ var now = time.Now()
 
 func TestMain(m *testing.M) {
 	address := "http://localhost:9200"
-	esConfig := elasticsearch7.Config{
-		Addresses: []string{address},
-	}
-	client, err := elasticsearch7.NewClient(esConfig)
-	if err != nil {
-		panic(err)
-	}
 	lg := zap.NewNop()
 
 	elst, err := elastic.NewClient(elastic.SetURL(address),
 		elastic.SetSniff(false),
 		elastic.SetHealthcheck(false))
+	if err != nil {
+		panic(err)
+	}
 
 	dbh = &esHandler{
-		client:  client,
 		cf:      conf.Configuration{},
 		log:     &logger.Logger{Logger: lg},
 		elastic: elst,
--- db/es.go
+++ db/es.go
@@ -1,18 +1,12 @@
 package db
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
-	elasticsearch7 "github.com/elastic/go-elasticsearch/v7"
-	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/jjauzion/ws-backend/conf"
 	logger "github.com/jjauzion/ws-backend/internal/logger"
 	"github.com/olivere/elastic/v7"
 	"go.uber.org/zap"
-	"io"
 	"io/ioutil"
 )
 
@@ -22,35 +16,10 @@ const (
 )
 
 type esHandler struct {
-	client  *elasticsearch7.Client
+	//client  *elasticsearch7.Client
 	log     *logger.Logger
 	cf      conf.Configuration
 	elastic *elastic.Client
-}
-
-type esSearchResponse struct {
-	Took     int  `json:"took"`
-	Time_out bool `json:"time_out"`
-	Shards   struct {
-		Total      int `json:"total"`
-		Successful int `json:"successful"`
-		Skipped    int `json:"skipped"`
-		Failed     int `json:"failed"`
-	} `json:"_shards"`
-	Hits struct {
-		Total struct {
-			Value    int    `json:"value"`
-			Relation string `json:"relation"`
-		} `json:"total"`
-		MaxScore float32 `json:"max_score"`
-		Hits     []struct {
-			Index  string      `json:"_index"`
-			Type   string      `json:"_type"`
-			Id     string      `json:"_id"`
-			Score  float32     `json:"_score"`
-			Source interface{} `json:"_source"`
-		} `json:"hits"`
-	} `json:"hits"`
 }
 
 func NewDBHandler(log *logger.Logger, cf conf.Configuration, elastic *elastic.Client) DatabaseHandler {
@@ -68,9 +37,7 @@ func NewDBHandler(log *logger.Logger, cf conf.Configuration, elastic *elastic.Cl
 }
 
 func (es *esHandler) Info() string {
-	info, _ := es.client.Info()
-	s := fmt.Sprint(info)
-	return s
+	return ""
 }
 
 func (es *esHandler) Bootstrap(ctx context.Context) error {
@@ -95,126 +62,9 @@ func (es *esHandler) Bootstrap(ctx context.Context) error {
 
 func (es *esHandler) new() error {
 	es.log.Info("connexion to ES cluster...")
-	address := es.cf.WS_ES_HOST + ":" + es.cf.WS_ES_PORT
-	esConfig := elasticsearch7.Config{
-		Addresses: []string{address},
-	}
-	var err error
-	es.client, err = elasticsearch7.NewClient(esConfig)
-	if err != nil {
-		return err
-	}
-	_, err = es.client.Info()
-	if err != nil {
-		return err
-	}
-	es.log.Info("successfully connected to ES", zap.String("host", address))
-	return err
-}
 
-func (es *esHandler) parseError(res *esapi.Response) (err error) {
-	var e map[string]interface{}
-	if err = json.NewDecoder(res.Body).Decode(&e); err != nil {
-		err = errors.New(fmt.Sprintf("error parsing the response body: %s", err))
-	} else {
-		err = errors.New(fmt.Sprintf("[%s] %s: %s",
-			res.Status(),
-			e["error"].(map[string]interface{})["type"],
-			e["error"].(map[string]interface{})["reason"],
-		))
-	}
-	return
-}
-
-func (es *esHandler) search(index []string, request io.Reader) (data []interface{}, err error) {
-	req := esapi.SearchRequest{
-		Index: index,
-		Body:  request,
-	}
-	var res *esapi.Response
-	if res, err = req.Do(context.Background(), es.client); err != nil {
-		return
-	}
-	if res.IsError() {
-		err = es.parseError(res)
-		return
-	}
-
-	var r esSearchResponse
-	if err = json.NewDecoder(res.Body).Decode(&r); err != nil {
-		err = errors.New(fmt.Sprintf("Error parsing the response body: %s", err))
-		return
-	}
-	if r.Shards.Failed > 0 {
-		es.log.Info("some shards failed during the search", zap.Int("number", r.Shards.Failed))
-	}
-	for i := 0; i < len(r.Hits.Hits); i++ {
-		data = append(data, r.Hits.Hits[i].Source)
-	}
-	return
-}
-
-func (es *esHandler) createIndex(name, mappingFile string) (err error) {
-	var mapping []byte
-	if mapping, err = ioutil.ReadFile(mappingFile); err != nil {
-		return
-	}
-	req := esapi.IndicesCreateRequest{
-		Index: name,
-		Body:  bytes.NewReader(mapping),
-	}
-	var res *esapi.Response
-	if res, err = req.Do(context.Background(), es.client); err != nil {
-		return
-	}
-	defer res.Body.Close()
-	if res.IsError() {
-		err = es.parseError(res)
-		return
-	}
-	return
-}
-
-func (es *esHandler) indexNewDoc(index string, reader io.Reader) (err error) {
-	req := esapi.IndexRequest{
-		Index: index,
-		Body:  reader,
-	}
-	var res *esapi.Response
-	if res, err = req.Do(context.Background(), es.client); err != nil {
-		return
-	}
-	if res.IsError() {
-		err = es.parseError(res)
-		return
-	}
-	defer res.Body.Close()
-	return
-}
-
-// WARNING: no error return if some doc failed
-// Should use esutil to get a control on the nb of success / fail:
-// https://github.com/elastic/go-elasticsearch/blob/master/_examples/bulk/indexer.go#L199
-func (es *esHandler) bulkIngest(index, file, refresh string) (err error) {
-	var bulk []byte
-	if bulk, err = ioutil.ReadFile(file); err != nil {
-		return
-	}
-	req := esapi.BulkRequest{
-		Index:   index,
-		Body:    bytes.NewReader(bulk),
-		Refresh: refresh,
-	}
-	var res *esapi.Response
-	if res, err = req.Do(context.Background(), es.client); err != nil {
-		return
-	}
-	defer res.Body.Close()
-	if res.IsError() {
-		err = es.parseError(res)
-		return
-	}
-	return
+	es.log.Info("successfully connected to ES")
+	return nil
 }
 
 type Itr func(*elastic.SearchHit) error
