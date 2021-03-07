7da7dc52e1262525bd74dab541abc58429fd4054
Karim BENHAMMOU <karim@benhammou.me>
2021-03-07 16:36:05 +0100

    task.go -> logging.go

--- db/logging.go
+++ db/logging.go
@@ -9,6 +9,9 @@ type Tasks []*Task
 func (t *Task) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
 	encoder.AddString("id", t.ID)
 	encoder.AddString("user_id", t.UserId)
+	encoder.AddTime("created_at", t.CreatedAt)
+	encoder.AddTime("ended_at", t.EndedAt)
+	encoder.AddTime("started_at", t.StartedAt)
 	return nil
 }
 
