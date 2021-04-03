7d5a06c93b8cff74c26b1f7cfa63c0a051897376
Karim BENHAMMOU <karim@benhammou.me>
2021-04-03 18:34:36 +0200

    restore playground

--- server/graphql_server.go
+++ server/graphql_server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"fmt"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
 	"github.com/jjauzion/ws-backend/db"
 	"net/http"
@@ -34,6 +35,9 @@ func RunGraphQL(bootstrap bool) {
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
 
 	router.Handle("/query", srv)
+	if app.conf.IS_DEV_ENV {
+		router.Handle("/playground", playground.Handler("GraphQL playground", "/query"))
+	}
 
 	resolver.Log.Info(fmt.Sprintf("connect to http://localhost:%s/playground for GraphQL playground", resolver.ApiPort))
 	resolver.Log.Error("", zap.Error(http.ListenAndServe(":"+resolver.ApiPort, router)))
